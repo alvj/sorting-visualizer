0562b6104c415700b457f366953ddcb43c8f60a2
Álvaro Pérez <alvarojperezperez@gmail.com>
2020-05-17 23:48:17 +0200

    Added animations and color for selection sort

--- sorting-visualizer.py
+++ sorting-visualizer.py
@@ -1,17 +1,21 @@
 import tkinter
 import random
+import time
 
 HEIGHT = 600
 BAR_WIDTH = 20
 LIST_LENGTH = 40
 OFFSET = 5
 
-def main():
-    random_list = create_random_list(LIST_LENGTH)
-    canvas = create_canvas((BAR_WIDTH + OFFSET) * LIST_LENGTH, HEIGHT)
-    
-    draw_list(canvas, random_list)
+# COLORS
+BLUE = "#c3dff7"
+RED = "#ff9c9c"
+GREEN = "#92fc94"
 
+def main():
+    canvas = create_canvas((BAR_WIDTH + OFFSET) * LIST_LENGTH, HEIGHT)
+    rect_list = draw_list(canvas)
+    selection_sort(canvas, rect_list)
     canvas.mainloop()
 
 
@@ -26,20 +30,43 @@ def create_canvas(width, height):
     return canvas
 
 
-def create_random_list(length):
-    random_list = []
-    for i in range(length):
-        random_list.append(random.randint(0, HEIGHT))
-    return random_list
-
-
-def draw_list(canvas, list):
-    for i in range(len(list)):
+def draw_list(canvas):
+    rect_list = []
+    for i in range(LIST_LENGTH):
         start_x = (i * BAR_WIDTH) + (OFFSET * i)
-        start_y = HEIGHT - list[i]
+        start_y = HEIGHT - random.randint(1, HEIGHT)
         end_x = start_x + BAR_WIDTH
         end_y = HEIGHT
-        canvas.create_rectangle(start_x, start_y, end_x, end_y, fill="#c3dff7")
+
+        rect = canvas.create_rectangle(
+            start_x,
+            start_y,
+            end_x,
+            end_y,
+            fill=BLUE,
+            width=0)
+        rect_list.append(rect)
+    return rect_list
+        
+def selection_sort(canvas, list):
+    smallest = HEIGHT - canvas.bbox(list[0])[1]
+    smallest_index = 0
+    for i in range(len(list)):
+        current = HEIGHT - canvas.bbox(list[i])[1]
+        canvas.itemconfig(list[i], fill=RED)
+        canvas.update()
+        if smallest > current:
+            canvas.itemconfig(list[i], fill=GREEN)
+            canvas.itemconfig(list[smallest_index], fill=BLUE)
+            smallest = current
+            smallest_index = i
+        else:
+            if smallest_index == i:
+                canvas.itemconfig(list[i], fill=GREEN)
+            else:
+                canvas.itemconfig(list[i], fill=BLUE)
+        time.sleep(0.3)
+
 
 
 if __name__ == '__main__':
